b42e2d677e1948130bd69520224852708680dab7
Frank Terbeck <ft@bewatermyfriend.org>
2021-08-11 22:40:48 +0200

    Read program from file specified on command line
    Signed-off-by: Frank Terbeck <ft@bewatermyfriend.org>

--- prg/Blueprint.hs
+++ prg/Blueprint.hs
@@ -1,17 +1,36 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# OPTIONS_GHC -fno-cse #-}
+
 module Main where
 
+import System.Console.CmdArgs
 import Control.Monad
 import Blueprint.Interpreter
+import Paths_blueprint (version)
+import Data.Version (showVersion)
 
-program =
-  unlines [ "(define fact (lambda (n)",
-            "               (if (< n 2)",
-            "                 1",
-            "                 (* n (fact (- n 1))))))",
-            "(fact 10)" ]
+data BlueprintArguments = BlueprintArguments
+  { verbose  :: Bool,
+    fileName :: String }
+  deriving (Show, Data, Typeable)
+
+config = cmdArgsMode $ BlueprintArguments {
+  verbose = False &= explicit
+          &= help "Enable verbose execution"
+          &= name "verbose"
+          &= name "v",
+  fileName = def
+             &= typ "SourceFile"
+             &= argPos 0
+  } &= summary ("Blueprint Scheme v" ++ showVersion version)
+    &= help    "Execute scheme programs"
+    &= program "blueprint"
+    &= helpArg [explicit, name "h", name "help"]
 
 main :: IO ()
 main = do
+  args <- cmdArgsRun config
+  program <- readFile $ fileName args
   putStrLn program
   putStr " → "
   print $ parseval program
